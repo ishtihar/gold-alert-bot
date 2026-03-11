c1ef73c9decb08346b24c3dfa3e4281c5611949d
Ishtihar Khan <ishtihar9@gmail.com>
2026-03-11 19:35:11 +0530

    Update gold_alert.py

--- gold_alert.py
+++ gold_alert.py
@@ -1,19 +1,32 @@
 import requests
-import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+# Tumhara bot token aur chat id
+BOT_TOKEN = "8788695706:AAHVdIupcHwvsT-xVmH3mEEgTbpaQg6o0zU"
+CHAT_ID = "649507132"
 
 def get_gold_price():
     url = "https://api.metals.live/v1/spot/gold"
     r = requests.get(url)
     data = r.json()
-    return data[0]['price']
+
+    # API se price nikalna
+    price = data[0]['price']
+    return price
+
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
     payload = {
         "chat_id": CHAT_ID,
         "text": msg
     }
+
     requests.post(url, data=payload)
+
+
+price = get_gold_price()
+
+message = f"Gold price now: ${price} per ounce"
+
+send_telegram(message)
